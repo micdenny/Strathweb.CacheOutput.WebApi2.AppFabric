2610bb9464b5dcccde07ebca6b68ff9bcaabf67d
Michael Denny <micdenny@gmail.com>
2016-03-04 12:53:19 +0100

    add nuspec and moved assembly version in a separated file

---
+++ Source/WebApi.OutputCache.AppFabric/Version.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// version number: <major>.<minor>.<non-breaking-feature>.<build>
+[assembly: AssemblyVersion("0.1.0.0")]
+
+// Note: until version 1.0 expect breaking changes on 0.X versions.
+
+// 0.1.0.0 First commit
